496bcc7830c3fce0c419f64efadae8afccefb7ec
KyleXu <kyle_x@foxmail.com>
2020-09-09 17:40:41 +0800

    【XTH】finished ex-2

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -1,3 +1,13 @@
+/*
+ * @Author: Kyle Xu
+ * @Date: 2020-09-09 16:30:44
+ * @LastEditors: Kyle Xu
+ * @LastEditTime: 2020-09-09 17:38:40
+ * @Blog: http://kylexu.cn
+ * @Github: https://github.com/xxxxxthhh
+ * @Mail: kyle_x@foxmail.com
+ * @Description:  // write sth about Code
+ */
 export default function countCharacter(string, prediction) {
   // This function will count the character number in a string which satisfies specific prediction.
   //
@@ -6,7 +16,17 @@ export default function countCharacter(string, prediction) {
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  if (string == '' || string == null ) {
-    
+  let countPrediction = 0;
+  if (string === '' || string == null || string === undefined) {
+    return 0;
   }
+  if (prediction === undefined) {
+    return string.length;
+  }
+  for (let i = 0; i < string.length; i += 1) {
+    if (prediction(string[i]) === true) {
+      countPrediction += 1;
+    }
+  }
+  return countPrediction;
 }
