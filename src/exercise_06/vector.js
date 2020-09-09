75bb103c49a8071667e3816373e49c7c56e505be
KyleXu <kyle_x@foxmail.com>
2020-09-10 00:43:41 +0800

    【XTH】finished ex-6

--- src/exercise_06/vector.js
+++ src/exercise_06/vector.js
@@ -1,3 +1,13 @@
+/*
+ * @Author: Kyle Xu
+ * @Date: 2020-09-09 16:30:44
+ * @LastEditors: Kyle Xu
+ * @LastEditTime: 2020-09-10 00:43:23
+ * @Blog: http://kylexu.cn
+ * @Github: https://github.com/xxxxxthhh
+ * @Mail: kyle_x@foxmail.com
+ * @Description:  // write sth about Code
+ */
 export default class Vector {
   // This class represents a 2-dimensional vector. Please implement the class according to the
   // following instructions:
@@ -13,4 +23,26 @@ export default class Vector {
   // class.
 
   // PLEASE DELETE THIS LINE AND ADD YOUR IMPLEMENTATION HERE
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    Object.defineProperty(this, 'x', {
+      writable: false,
+    });
+    Object.defineProperty(this, 'y', {
+      writable: false,
+    });
+  }
+
+  distance() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+  
+  static plus(V1, V2) {
+    return new Vector(V1.x + V2.x, V1.y + V2.y);
+  }
+
+  static minus(V1, V2) {
+    return new Vector(V1.x - V2.x, V1.y - V2.y);
+  }
 }
